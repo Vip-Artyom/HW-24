e6d60614e6e1acc333680fcb5eced3185766d309
Артём <snowboard_team@mail.ru>
2022-07-19 17:14:46 +0600

    finish

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_restx import abort
 from utils import create_query
 
@@ -10,7 +10,7 @@ DATA_DIR = os.path.join(BASE_DIR, "data")
 
 
 @app.route("/perform_query")
-def perform_query():
+def perform_query() -> Response:
     cmd_1 = request.args.get("cmd1")
     value_1 = request.args.get("value1")
     cmd_2 = request.args.get("cmd2")
@@ -20,22 +20,20 @@ def perform_query():
     if not (cmd_1 and value_1 and file_name):
         abort(400)
 
-    file_path = os.path.join(DATA_DIR, file_name)
+    file_path = os.path.join(DATA_DIR, str(file_name))
 
     if not os.path.exists(file_path):
         return abort(400, "Wrong file_name")
 
     with open(file_path) as file:
 
-        res = create_query(cmd_1, value_1, file)
+        res = create_query(str(cmd_1), str(value_1), file)
 
-        res = "\n".join(res)
-        print(res)
         if cmd_2 and value_2:
             print('*'*10)
-            res = create_query(cmd_2, value_2, res)
+            res = create_query(str(cmd_2), str(value_2), iter(res))
 
-    return app.response_class(res, content_type="text/plain")
+    return app.response_class("\n".join(res), content_type="text/plain")
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,18 +1,25 @@
-def create_query(cmd, value, file_list):
+import re
+from typing import Iterator, List, Any
+
+
+def create_query(cmd: str, value: str, file_list: Iterator) -> List[Any]:
     if cmd == "filter":
-        result = filter(lambda y: value.upper() in y, file_list)
+        result = list(filter(lambda y: value.upper() in y, file_list))
         return result
     if cmd == "map":
-        value = int(value)
-        result = map(lambda x: x.split()[value], file_list)
+        result = list(map(lambda x: x.split()[int(value)], file_list))
         return result
     if cmd == "unique":
         return list(set(file_list))
     if cmd == "sort":
         reverse = "value" == "desc"
-        res = sorted(file_list, reverse=reverse)
+        res = list(sorted(file_list, reverse=reverse))
         return res
     if cmd == "limit":
-        value = int(value)
-        res = list(file_list)[:value]
+        res = list(file_list)[:int(value)]
         return res
+    if cmd == "regex":
+        regex = re.compile(value)
+        res = list(filter(lambda x: regex.findall(x), file_list))
+        return res
+    return []
